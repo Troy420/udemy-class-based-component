a4ceaa23eaef037456df821b4040a0018f98289c
Bernard <bernardy_ho@hotmail.com>
2022-06-07 20:07:14 +0700

    learning class-based component lifestyle

---
+++ src/components/UserFinder.js
@@ -0,0 +1,58 @@
+import { Fragment, Component } from "react";
+
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={searchChangeHandler} />
+        </div>
+        <Users users={filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
+
+export default UserFinder;
