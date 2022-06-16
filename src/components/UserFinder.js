9b99ab9a960d0bb08f78ab4e959e165fe5323a13
Bernard <bernardy_ho@hotmail.com>
2022-06-16 19:42:15 +0700

    error boundaries

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,30 +2,52 @@ import { Fragment, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
 
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      filteredUsers: this.context.users,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });
   }
 
   render() {
     return (
       <Fragment>
         <div className={classes.finder}>
-          <input type="search" onChange={searchChangeHandler} />
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
